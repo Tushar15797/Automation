b1c1e088937ab3d2f5dd4b8e6a56758d25fce511
Tushar15797 <tsp15797@gmail.com>
2024-07-26 19:20:26 +0530

    Lab 17 Push

--- Lab17.java
+++ Lab17.java
@@ -4,5 +4,7 @@ public class Lab17 {
     public static void main(String[] args) {
         System.out.println("GIt Hubb  to Pull Code.....");
     }
-
+    public static void main1() {
+        System.out.println("Hi");
+    }
 }
