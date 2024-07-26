30a114eaf9260a8e38c198b15f598d9829c63d4d
Tushar15797 <tsp15797@gmail.com>
2024-07-26 13:02:03 +0530

    Lab 17 Push

--- Lab17.java
+++ Lab17.java
@@ -2,6 +2,7 @@ package A212024;
 
 public class Lab17 {
     public static void main(String[] args) {
-        System.out.println("GIt Hub");
+        System.out.println("GIt Hubb  to Pull Code.....");
     }
+
 }
