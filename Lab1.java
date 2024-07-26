b7fccce3ff655cddd259f6450288c5595d0168a9
Tushar15797 <tsp15797@gmail.com>
2024-07-26 19:26:29 +0530

    Lab 1 Push

--- Lab1.java
+++ Lab1.java
@@ -4,6 +4,7 @@ public class Lab1 {
     public static void main(String[] args) {
         for(int i=1;i<=5;++i){
             System.out.println(i);
+            System.out.println("123");
         }
     }
 }
