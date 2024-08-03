d74a9d63446db4f5a94e899f5018e584a3c07c53
Tushar15797 <tsp15797@gmail.com>
2024-08-04 00:24:08 +0530

    Push

--- Lab1.java
+++ Lab1.java
@@ -2,6 +2,7 @@ package A21072024;
 
 public class Lab1 {
     public static void main(String[] args) {
+        //Loops Program
         for (int i=2024;i<3000;i++) {
             if (i%4==0) {
                 System.out.println(i);
