3d2eea048957e469c5b7c2783d3443115fbdfdd0
Tushar15797 <tsp15797@gmail.com>
2024-10-13 00:10:43 +0530

    Selenium Code

---
+++ src/test/java/Ex01.java
@@ -0,0 +1,15 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Ex01 {
+    public static void main(String[] args) throws InterruptedException {
+        ChromeDriver driver=new ChromeDriver();
+        driver.get("https://app.vwo.com");
+        driver.manage().window().maximize();
+        Thread.sleep(5000);
+        driver.findElement(By.xpath("(//input[@type='email'])[1]")).sendKeys("tsp15797@gmail.com");
+        driver.findElement(By.id("login-password")).sendKeys("123456");
+        driver.findElement(By.xpath("(//button[@type='submit'])[1]")).click();
+
+    }
+}
