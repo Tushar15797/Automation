8b23cdd976ccd4242ad27195d11a03246b8823db
Tushar15797 <tsp15797@gmail.com>
2024-10-13 00:17:16 +0530

    Update Ex01.java

--- src/test/java/Ex01.java
+++ src/test/java/Ex01.java
@@ -3,6 +3,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Ex01 {
     public static void main(String[] args) throws InterruptedException {
+        
         ChromeDriver driver=new ChromeDriver();
         driver.get("https://app.vwo.com");
         driver.manage().window().maximize();
