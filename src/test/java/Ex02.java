d7f912ab311404cf213a22f28f5d13b48752ef5a
Tushar15797 <tsp15797@gmail.com>
2024-10-13 18:10:43 +0530

    selenium code

---
+++ src/test/java/Ex02.java
@@ -0,0 +1,12 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.Test;
+
+public class Ex02 {
+    @Test
+    public void a(){
+        WebDriver driver=new EdgeDriver();
+        driver.get("https://app.vwo.com");
+
+    }
+}
